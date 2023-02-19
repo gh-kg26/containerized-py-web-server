bd165edea8db0013e915d145cf27bbe1504d0e91
Kushagra Gupta <kg@Kushagras-MacBook-Air.local>
2023-02-20 01:33:20 +0530

    Task 1 Completed successfully

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-FROM python:3
-ADD index.html index.html
-ADD server.py server.py
-EXPOSE 8888
-ENTRYPOINT ["python3", "server.py"]
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+COPY server.py .
+
+EXPOSE 8000
+
+CMD ["python", "server.py"]
--- server.py
+++ server.py
@@ -1,29 +1,17 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer, ThreadingHTTPServer
-import time
-import json
-from socketserver import ThreadingMixIn
-import threading
-hostName = '0.0.0.0'
-serverPort = 80
-class Handler(BaseHTTPRequestHandler):
- def do_GET(self):
-  if self.path == '/':
-    self.send_response(200)
-    self.send_header("content-type", "text/html")
-    self.end_headers()
-    content = open("index.html", "rb").read()
-    self.wfile.write(content)
-  else:
-    self.send_response(404)
-    return
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    if __name__ == "__main__":
-        webServer = ThreadingHTTPServer((hostName, serverPort), Handler)
-        print ("Server started http://%s:%s" % (hostName, serverPort))
-        try:
-            webServer.serve_forever()
-        except KeyboardInterrupt:
-            pass
-        webServer.server_close()
-        print("Server stopped.")
-    
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
+
+def run(server_class=HTTPServer, handler_class=MyServer, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
